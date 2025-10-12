50c8bbf0f896aa49901f206b37ab3b419eb85515
Fatin Ishtiak <122414653+FatinIshtiak05@users.noreply.github.com>
2025-10-12 22:27:50 +0600

    Create conditionals.js

---
+++ Basics/Conditionals/conditionals.js
@@ -0,0 +1,15 @@
+/*1️⃣ Conditionals — if, else if, else
+
+Conditionals let your program decide what to do based on conditions (true/false).
+
+*/
+
+let score = 85;
+
+if (score >= 90) {
+  console.log("Excellent!");
+} else if (score >= 60) {
+  console.log("Good Job!");
+} else {
+  console.log("Try Again!");
+}
