7aee79d6b5163d1e71a3a1f9afddc67cca956c87
Fatin Ishtiak <122414653+FatinIshtiak05@users.noreply.github.com>
2025-10-13 21:37:53 +0600

    Create map.js

---
+++ Basics/ Array methods: map, filter, reduce, forEach/map.js
@@ -0,0 +1,9 @@
+/*1. map() — transforms elements & returns a new array
+It runs a function on every element and returns a new array with the results.*/
+
+let numbers = [1, 2, 3, 4];
+let doubled = numbers.map((num) => num * 2);
+console.log(doubled); // [2, 4, 6, 8]
+
+//Use map() when you want to create a new array based on some transformation.
+
