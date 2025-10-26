af105c3ef3bae89d72fa0fc4efd8f857ad861693
Fatin Ishtiak <122414653+FatinIshtiak05@users.noreply.github.com>
2025-10-26 20:10:45 +0600

    Add files via upload

---
+++ Intermediate/DOM Manipulation (Selectors, Events, Forms)/changingele.js
@@ -0,0 +1,9 @@
+/*2️⃣ Changing Elements
+
+You can modify text, HTML, styles, and attributes.*/
+
+title.textContent = "Hello, JavaScript!";
+title.style.color = "blue";
+title.style.fontSize = "28px";
+
+message.innerHTML = "<b>Learning DOM Manipulation!</b>";
